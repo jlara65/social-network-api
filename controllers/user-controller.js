64f3e04ac8ef3fa9cf9472f15130701b53b5751a
Joseph Lara <vipercar233@gmail.comgit config --global user.name Joseph>
2022-11-09 22:42:42 -0600

    created User model and controller

---
+++ controllers/user-controller.js
@@ -0,0 +1,15 @@
+const { User } = require('../models');
+const { db } = require('../models/User');
+
+const userController = {
+  getAllUser(req, res) {
+    User.find({})
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+};
+
+module.exports = userController;
---
+++ models/User.js
@@ -0,0 +1,29 @@
+const { Schema, model, Types } = require('mongoose');
+
+const validateEmail = (email) => {
+  const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+};
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: 'Email address is require',
+    validate: [validateEmail, 'Please fill a valid email address'],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please fill a valid email address',
+    ],
+  },
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
